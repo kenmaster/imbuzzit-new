ed4124d9959c73f06ece4699f3b23a84e2b008cc
Frantz DARIBO <frantz.daribo@openbridge.fr>
2013-03-28 18:35:53 +0100

    Encodage du pwd dans la fixture user

--- src/Imbuzzit/ApiBundle/DataFixtures/ORM/LoadUserData.php
+++ src/Imbuzzit/ApiBundle/DataFixtures/ORM/LoadUserData.php
@@ -4,10 +4,25 @@ namespace Imbuzzit\ApiBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Imbuzzit\ApiBundle\Entity\User;
 
-class LoadUserData implements FixtureInterface
+class LoadUserData implements FixtureInterface, ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -15,7 +30,11 @@ class LoadUserData implements FixtureInterface
     {
         $user = new User();
         $user->setUsername('admin');
-        $user->setPassword('test');
+        $encoder = $this->container
+            ->get('security.encoder_factory')
+            ->getEncoder($user)
+        ;
+        $user->setPassword($encoder->encodePassword('secret', $user->getSalt()));
         $user->setEmail('contact@imbuzzit.com');
         $datetime = new \DateTime();
         $user->setBirthday($datetime);
